64c4feb88a954a5f3d1d99b4996abd11daa68765
tyrrell-IH <gotothertm@gmail.com>
2026-05-18 10:04:26 +0900

    使用しないテストを一旦コメントアウト

--- test/controllers/discord_messages_controller_test.rb
+++ test/controllers/discord_messages_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
-
-class DiscordMessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get discord_messages_index_url
-    assert_response :success
-  end
-end
+# require "test_helper"
+#
+# class DiscordMessagesControllerTest < ActionDispatch::IntegrationTest
+#   test "should get index" do
+#     get discord_messages_index_url
+#     assert_response :success
+#   end
+# end
